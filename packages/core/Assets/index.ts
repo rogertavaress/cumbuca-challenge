770f507b9935d9b46c1b8ace8ade625ffeb158f4
Rogério Tavares <email@rogeriotavares.dev>
2024-02-12 18:52:46 -0300

    feature: add assets

---
+++ packages/core/Assets/index.ts
@@ -0,0 +1,19 @@
+import BackIcon from "./Back.png";
+import MinusIcon from "./Minus.png";
+import MinusWhiteIcon from "./MinusWhite.png";
+import PlusIcon from "./Plus.png";
+import PlusWhiteIcon from "./PlusWhite.png";
+import SearchIcon from "./Search.png";
+import SettingsIcon from "./Settings.png";
+import TrashIcon from "./Trash.png";
+
+export {
+  BackIcon,
+  MinusIcon,
+  MinusWhiteIcon,
+  PlusIcon,
+  PlusWhiteIcon,
+  SearchIcon,
+  SettingsIcon,
+  TrashIcon,
+};
