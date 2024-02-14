b5f982bd8c92d38adc65d1d97ffb605b227909e3
Rogério Tavares <email@rogeriotavares.dev>
2024-02-13 21:07:47 -0300

    feature: darkmode assets

--- packages/core/Assets/index.ts
+++ packages/core/Assets/index.ts
@@ -1,19 +1,25 @@
 import BackIcon from "./Back.png";
+import BackWhiteIcon from "./BackWhite.png";
+import MenuIcon from "./Menu.png";
+import MenuWhiteIcon from "./MenuWhite.png";
 import MinusIcon from "./Minus.png";
 import MinusWhiteIcon from "./MinusWhite.png";
 import PlusIcon from "./Plus.png";
 import PlusWhiteIcon from "./PlusWhite.png";
 import SearchIcon from "./Search.png";
-import SettingsIcon from "./Settings.png";
+import SearchWhiteIcon from "./SearchWhite.png";
 import TrashIcon from "./Trash.png";
 
 export {
   BackIcon,
+  BackWhiteIcon,
+  MenuIcon,
+  MenuWhiteIcon,
   MinusIcon,
   MinusWhiteIcon,
   PlusIcon,
   PlusWhiteIcon,
   SearchIcon,
-  SettingsIcon,
+  SearchWhiteIcon,
   TrashIcon,
 };
