3c1597c8d1b49e4e8a8a1175dc88f1fc186c052b
Rogério Tavares <email@rogeriotavares.dev>
2024-02-13 21:12:18 -0300

    feature: biometry hook

--- packages/core/Libs/Biometry/index.ts
+++ packages/core/Libs/Biometry/index.ts
@@ -1,4 +1,11 @@
 import * as LocalAuthentication from "expo-local-authentication";
+import { useCallback, useEffect, useState } from "react";
+
+import {
+  changeSettings,
+  getSettings,
+} from "../../Storage/repositories/settings.repository";
+import { SettingsKeyEnum } from "../../Storage/entities/settings.entity";
 
 export const biometry = async () => {
   const { success } = await LocalAuthentication.authenticateAsync({
@@ -9,3 +16,37 @@ export const biometry = async () => {
 
 export const hardwareBiometry = async () =>
   LocalAuthentication.hasHardwareAsync();
+
+export const useBiometry = () => {
+  const [isEnabled, setIsEnabled] = useState<boolean>();
+
+  const getBiometry = async () => {
+    try {
+      const [hasBiometry, { biometry }] = await Promise.all([
+        hardwareBiometry(),
+        getSettings(),
+      ]);
+      setIsEnabled(hasBiometry && biometry);
+    } catch (err) {
+      setIsEnabled(false);
+    }
+  };
+
+  const changeBiometry = useCallback(async (updatedIsEnabled: boolean) => {
+    const hasBiometry = await hardwareBiometry();
+    if (!hasBiometry) {
+      return;
+    }
+    setIsEnabled(updatedIsEnabled);
+    changeSettings(SettingsKeyEnum.BIOMETRY, updatedIsEnabled);
+  }, []);
+
+  useEffect(() => {
+    getBiometry();
+  }, []);
+
+  return {
+    isBiometryEnabled: isEnabled,
+    changeBiometry,
+  };
+};
