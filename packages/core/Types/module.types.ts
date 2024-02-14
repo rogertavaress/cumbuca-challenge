1067213fc2d8ee84c759967560b104374745ffa9
Rogério Tavares <email@rogeriotavares.dev>
2024-02-13 21:17:13 -0300

    chore: change imports

--- packages/core/Types/module.types.ts
+++ packages/core/Types/module.types.ts
@@ -1,4 +1,4 @@
-import { NavigationProviderType } from "../Navigation/navigation.types";
+import { NavigationProviderType } from "@core/Navigation/types";
 
 export type ModuleType = {} & Omit<NavigationProviderType, "initialScreen">;
 
